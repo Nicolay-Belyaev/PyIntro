da2e866faa978cae35ef4d5e7dd313a5d9872f9c
Psina linux <mr.samoylove@gmail.com>
2022-09-30 14:53:31 +0300

    Added import & export to GUI

--- Seminar_07/Homework_Phonebook/main.py
+++ Seminar_07/Homework_Phonebook/main.py
@@ -6,6 +6,5 @@ import controller
 
 # здесь мы сами решаем, запускать GUI или работать с консолью
 
-controller.logic_using_console()
-# controller.logic_using_gui()
-
+#  controller.logic_using_console()
+controller.logic_using_gui()
